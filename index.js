52e14499aec153d9b1b572c259a7687a1341aa70
Shahed Rahman <shahed.rahman88@gmail.com>
2020-08-15 20:08:58 +0600

    modify sign up

---
+++ index.js
@@ -0,0 +1,7 @@
+function myFunction() {
+  let signUp = document.getElementById("box1");
+  let signIn = document.getElementById("box2");
+
+  signUp.style.display = box1.style.display == "none" ? "block" : "none";
+  signIn.style.display = box2.style.display == "none" ? "block" : "none";
+}
